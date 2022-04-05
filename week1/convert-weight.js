9c05389c975d05095b87879f73f6bab0e31ac667
Brian Mosher <mosher.brian@gmail.com>
2022-04-05 14:49:37 +0000

    Add parseFloat() to sanitize function args

--- week1/convert-weight.js
+++ week1/convert-weight.js
@@ -1,27 +1,27 @@
 const convertWeight = (function () {
 
 	function gramsToKilograms(g) {
-		return g / 1000;
+		return parseFloat(g)/ 1000;
 	}
 
 	function gramsToMilligrams(g) {
-		return g * 1000;
+		return parseFloat(g)* 1000;
 	}
 
 	function kilogramsToGrams(kg) {
-		return kg * 1000;
+		return parseFloat(kg) * 1000;
 	}
 
 	function kilogramsToMilligrams(kg) {
-		return kg * 1000 * 1000;
+		return parseFloat(kg) * 1000 * 1000;
 	}
 
 	function milligramsToGrams(mg) {
-		return mg / 1000;
+		return parseFloat(mg) / 1000;
 	}
 
 	function milligramsToKilograms(mg) {
-		return mg / 1000 / 1000;
+		return parseFloat(mg) / 1000 / 1000;
 	}
 
 	return {
