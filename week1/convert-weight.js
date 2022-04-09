207873822642c97be763edb2381f556fe5b44679
Brian Mosher <mosher.brian@gmail.com>
2022-04-09 17:42:29 +0000

    Merge

--- week1/convert-weight.js
+++ week1/convert-weight.js
@@ -23,6 +23,24 @@ const Weight = (function () {
 		return this.weight;
 	};
 
+	Constructor.prototype.addGrams = function (g) {
+		units = 'g';
+		this.weight += parseFloat(g) * 1000;
+		return this;
+	};
+
+	Constructor.prototype.addKg = function (kg) {
+		units = 'kg';
+		this.weight += parseFloat(kg) * 1000 * 1000;
+		return this;
+	};
+
+	Constructor.prototype.addMg = function (mg) {
+		units = 'mg';
+		this.weight += parseFloat(mg);
+		return this;
+	};
+
 	Constructor.prototype.units = function () {
 		return units;
 	};
