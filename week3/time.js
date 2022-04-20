1ceddb2d1cbec06d8e04dd9a3d4924cbf53b6c2d
Brian Mosher <mosher.brian@gmail.com>
2022-04-20 17:58:25 +0000

    Complete Week3 - Wednesday project

--- week3/time.js
+++ week3/time.js
@@ -8,56 +8,82 @@
 // time.addYears() - add a number of years to a date.
 
 function Constructor(...dateArgs) {
-	this.date = new Date(...dateArgs);
+	let lastArg = dateArgs?.[dateArgs.length - 1];
+	let settings = lastArg?.days || lastArg?.month ? dateArgs.pop() : null;
+	settings = Object.assign(
+		{
+			days: [
+				'Sunday',
+				'Monday',
+				'Tuesday',
+				'Wednesday',
+				'Thursday',
+				'Friday',
+				'Saturday'
+			],
+			months: [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December'
+			]
+		},
+		settings ? settings : {}
+	);
+
+	Object.freeze(settings);
+	Object.defineProperties(this, {
+		date: {value: new Date(...dateArgs)},
+		_settings: {value: settings}
+	});
 }
 
 Constructor.prototype.getDay = function () {
-	return [
-		'Sunday',
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday'
-	][this.date.getDay()];
+	return this._settings.days[this.date.getDay()];
 };
 
 Constructor.prototype.getMonth = function () {
-	return [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December'
-	][this.date.getMonth()];
+	return this._settings.months[this.date.getMonth()];
 };
 
 Constructor.prototype.addSeconds = function (seconds) {
-	this.date.setTime(seconds * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		seconds * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 Constructor.prototype.addMinutes = function (minutes) {
-	this.date.setTime(minutes * 60 * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		minutes * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 Constructor.prototype.addHours = function (hours) {
-	this.date.setTime(hours * 60 * 60 * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		hours * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 Constructor.prototype.addDays = function (days) {
-	this.date.setTime(days * 24 * 60 * 60 * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		days * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 Constructor.prototype.addMonths = function (months) {
@@ -66,13 +92,19 @@ Constructor.prototype.addMonths = function (months) {
 	for (let i = 1; i <= months; i++) {
 		daysToAdd += monthDays[(this.date.getMonth() + i) % 12];
 	}
-	this.date.setTime(daysToAdd * 24 * 60 * 60 * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		daysToAdd * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 Constructor.prototype.addYears = function (years) {
-	this.date.setTime(years * 365 * 24 * 60 * 60 * 1000 + this.date.getTime());
-	return this;
+	let newTime = new Constructor(
+		years * 365 * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return newTime;
 };
 
 export default Constructor;
