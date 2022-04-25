d5e02f5a23a48e8e79a3357481bdd6152252676b
Brian Mosher <mosher.brian@gmail.com>
2022-04-25 15:37:34 +0000

    Complete Week 4 - Monday project

---
+++ week4/time.js
@@ -0,0 +1,131 @@
+// time.getDay() - get the day of the week as a string.
+// time.getMonth() - get the month of the year as a string.
+// time.addSeconds() - add a number of seconds to a date.
+// time.addMinutes() - add a number of minutes to a date.
+// time.addHours() - add a number of hours to a date.
+// time.addDays() - add a number of days to a date.
+// time.addMonths() - add a number of months to a date.
+// time.addYears() - add a number of years to a date.
+
+/**
+ * Emit a custom event
+ * @param  {String} type   The event type
+ * @param  {Object} detail Any details to pass along with the event
+ * @param  {Node}   elem   The element to attach the event to
+ */
+function emitEvent(type, detail = {}, elem = document) {
+	// Make sure there's an event type
+	if (!type) return;
+
+	// Create a new event
+	let event = new CustomEvent(type, {
+		bubbles: true,
+		cancelable: true,
+		detail: detail
+	});
+
+	// Dispatch the event
+	return elem.dispatchEvent(event);
+}
+
+function Constructor(...dateArgs) {
+	let lastArg = dateArgs?.[dateArgs.length - 1];
+	let settings = lastArg?.days || lastArg?.month ? dateArgs.pop() : null;
+	settings = Object.assign(
+		{
+			days: [
+				'Sunday',
+				'Monday',
+				'Tuesday',
+				'Wednesday',
+				'Thursday',
+				'Friday',
+				'Saturday'
+			],
+			months: [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December'
+			]
+		},
+		settings ? settings : {}
+	);
+
+	Object.freeze(settings);
+	Object.defineProperties(this, {
+		date: {value: new Date(...dateArgs)},
+		_settings: {value: settings}
+	});
+}
+
+Constructor.prototype.getDay = function () {
+	return this._settings.days[this.date.getDay()];
+};
+
+Constructor.prototype.getMonth = function () {
+	return this._settings.months[this.date.getMonth()];
+};
+
+Constructor.prototype.addSeconds = function (seconds) {
+	let newTime = new Constructor(
+		seconds * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+Constructor.prototype.addMinutes = function (minutes) {
+	let newTime = new Constructor(
+		minutes * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+Constructor.prototype.addHours = function (hours) {
+	let newTime = new Constructor(
+		hours * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+Constructor.prototype.addDays = function (days) {
+	let newTime = new Constructor(
+		days * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+Constructor.prototype.addMonths = function (months) {
+	const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 31, 31, 30, 31];
+	let daysToAdd = 0;
+	for (let i = 1; i <= months; i++) {
+		daysToAdd += monthDays[(this.date.getMonth() + i) % 12];
+	}
+	let newTime = new Constructor(
+		daysToAdd * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+Constructor.prototype.addYears = function (years) {
+	let newTime = new Constructor(
+		years * 365 * 24 * 60 * 60 * 1000 + this.date.getTime(),
+		this._settings
+	);
+	return emitEvent('time:update', {time: newTime}) ? newTime : this;
+};
+
+export default Constructor;
