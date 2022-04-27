6f0bdc211c63b34a1d9397e9154a065e63d17dc3
Brian Mosher <mosher.brian@gmail.com>
2022-04-27 15:44:15 +0000

    Complete Week4 - Wednesday

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,20 @@
-export default {
-	input: 'week4/time.js',
-	output: {
-		file: 'build/time.js',
-		format: 'iife',
-		name: 'Time'
-	}
-};
+import pkg from './package.json';
+
+const banner = `/*! ${pkg.name} v${pkg.version} | ${
+	pkg.description
+} | Copyright ${new Date().getFullYear()} | ${pkg.license} license */`;
+
+const formats = ['iife', 'es', 'cjs'];
+
+export default formats.map(function (format) {
+	return {
+		input: 'week4/time.js',
+		output: {
+			file: `build/time${format === 'iife' ? '' : `.${format}`}.js`,
+			format: format,
+			name: 'Time',
+			banner: banner,
+			exports: 'default'
+		}
+	};
+});
